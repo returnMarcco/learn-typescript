6a904402fa45f2739e1d98ad45fbf2d0af679fbf
returnMarcco <jason.vellucci@live.com>
2024-01-20 05:56:16 +1100

    practice typed Arrays, Tuples, Object Literals

--- learn-typescript.js
+++ learn-typescript.js
@@ -1,3 +1,12 @@
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 console.log('Hello, World!');
 console.log('Learning TypeScript');
 // Typed Vars/Data Structures.
@@ -8,7 +17,27 @@ var myBoolTrue = true;
 var myBoolFalse = false;
 var myNumArray = [0, 1, 2, 3, 4];
 var myStrArray = ['return', 'Marcco'];
-// Typed Object Literal here.
+var myReadOnlyStrArr = ['test', 'fixed'];
+// myReadOnlyStrArr.push('thisWillErrorAtCompileTime');
+// Type-enforced to the initial types passed into the declaration.
+var myTuple = [1, 'Hello', true];
+console.log(myTuple);
+// Make the Tuple readonly because any values after the initial ones cannot be type-enforced.
+// Named tuples - provides context on index-values.
+var myReadOnlyTuple = __spreadArray([], myTuple, true); // Spread - copies by value.
+console.log(myReadOnlyTuple);
+var myCar = {
+    vehicleId: 1,
+    model: 'Ford',
+    year: new Date().getFullYear(),
+    // buildDate is optional
+};
+console.log(myCar);
+// Index Signatures: Can define object without defining list of keys
+var myRandomObj = {}; // All keys must be a string and values but of types string OR number.
+myRandomObj.id = 1;
+myRandomObj.model = 'Holden';
+console.log(myRandomObj);
 // Typed Functions. (params)
 function myTypedSumFunc(x, y) {
     return x + y;
--- learn-typescript.ts
+++ learn-typescript.ts
@@ -1,8 +1,8 @@
 console.log('Hello, World!');
 console.log('Learning TypeScript');
 
-
-// Typed Vars/Data Structures.
+// Typed Vars
+// -------------
 let myInt: number   = 5;
 let myFloat: number = 7.5;
 
@@ -11,14 +11,41 @@ let myStr: string = 'A typed string';
 let myBoolTrue: boolean = true;
 let myBoolFalse: boolean = false;
 
+
+// Data Structures / Special Types
+// ----------------------------------
 const myNumArray: number[] = [0, 1, 2, 3, 4];
 const myStrArray: string[] = ['return', 'Marcco'];
 
-// Typed Object Literal here.
+const myReadOnlyStrArr: readonly string[] = ['test', 'fixed'];
+// myReadOnlyStrArr.push('thisWillErrorAtCompileTime');
 
+// Type-enforced to the initial types passed into the declaration.
+const myTuple: [number, string, boolean] = [1, 'Hello', true];
+console.log(myTuple);
+
+// Make the Tuple readonly because any values after the initial ones cannot be type-enforced.
+// Named tuples - provides context on index-values.
+const myReadOnlyTuple: readonly [id: number, msg: string, isDeleted: boolean] = [...myTuple]; // Spread - copies by value.
+console.log(myReadOnlyTuple);
+
+const myCar: { vehicleId: number, model: string, year: number, buildDate?: Date } = {
+    vehicleId: 1,
+    model: 'Ford',
+    year: new Date().getFullYear(),
+    // buildDate is optional
+};
+console.log(myCar);
+
+// Index Signatures: Can define object without defining list of keys
+const myRandomObj: { [index: string]: string | number } = {}; // All keys must be a string and values but of types string OR number.
+myRandomObj.id = 1;
+myRandomObj.model = 'Holden';
+console.log(myRandomObj);
 
 
 // Typed Functions. (params)
+// ----------------------------
 function  myTypedSumFunc(x: number, y: number) {
     return x + y;
 }
